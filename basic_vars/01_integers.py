fbdd66a1bf9df16d747f6be8aebec8e8ab2113cd
Deeplo0905 <68748791+Deeplo0905@users.noreply.github.com>
2021-07-24 23:59:22 -0400

    added the first basic variable file

---
+++ basic_vars/01_integers.py
@@ -0,0 +1,52 @@
+
+# usually, there are three parts to a variable
+# 1. name = how you reference the variable (the name is chosen by the programmer)
+# 2. type = says what values the variable can have (the type is determined from the value)
+# 3. value = the actual value stored in the variable (the value is chosen by the programmer)
+#            a value can be provided from a different variable (y = x) or provided as a constant (y = 5)
+
+# For example:
+# x = 5
+# name: x
+# type: int
+# value: 5
+
+# Another example:
+# s = "greetings, sire"
+# name: s
+# type: string
+# value: "greetings, sire"
+
+# PRACTICE STARTS HERE
+
+# DEFINING
+
+# 1. define a variable with name 'x' and value 5
+# print the type of this variable by calling the built-in type(<var>) function and printing the result
+
+# 2. define a variable with name 'y' and value 17
+# print the value of this variable by calling the built-in print(<var>)
+
+# 3. define a variable with name 'z' and a value that is a prime number
+
+# 4. define a variable with name 'sum' that is the sum of x, y, and z
+# print the type and value of this variable
+
+# 5. REDEFINE the variable with name 'x' and give it value 25.5
+# print the type of this variable
+
+# 6. REDEFINE the variable with name 'y' and give it value of it's current value plus 1.0
+# print the type of this variable and print its value
+
+# 7. REDEFINE the variable with name 'sum' and give it value of the 'x' and 'y' values
+# print the type and value of this variable
+
+# 8. CHALLENGE: swap the values in 'x' and 'y' ('x' is redefined to have 'y' value and 'y' is redefined to have 'x' value)
+#               CONSTRAINT: DO NOT explicitly set the variables with constants like y = 25.5
+#                           your code can not use any constants (any method that does not use constants is valid)
+
+
+# NOTE:
+# - a BUILT-IN function is automatically provided in any python program
+#   (that means that you don't need to define it), you can just call it
+# - REDEFINING a variable that is already defined sets the variable to a new value (won't be able to use whatever the old value was)
