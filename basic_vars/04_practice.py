642654d5722cd8adad6f7cfd6815582c2d64c0a9
Deeplo0905 <68748791+Deeplo0905@users.noreply.github.com>
2021-07-28 10:09:01 -0400

    added more practice problems for basic vars

--- basic_vars/04_practice.py
+++ basic_vars/04_practice.py
@@ -14,13 +14,14 @@
 #       3. adds the second number to the result of step 2
 #       4. print True if the result is less than 15 or greater than 25, False otherwise
 
-# write code that
-#       0. takes in three numbers (a, b, and c in order) with three calls to input(...) function
+# write code that DETECTS RIGHT TRIANGLES
+#       0. take in three numbers (x, y, and z in order) with three calls to input(...) function
 #       1. converts all three to integers
 #       2. prints True if any ordering of the numbers satisfies a right triangle False otherwise
+#       NOTE: you will have to see if the Pythagorean theorem is satisfied three times!
 
-# write code that
-#       0. takes in four numbers (x0, y0) and (x1, y1) with four calls to input(...) function
+# write code that FINDS THE LINE BETWEEEN TWO POINTS
+#       0. take in four numbers (x0, y0) and (x1, y1) with four calls to input(...) function
 #       1. converts all four to integers
 #       2. calculates the slope between points (x0, y0) and (x1, y1)
 #       3. calculates the x-intercept of the line going through points (x0, y0) and (x1, y1)
@@ -30,10 +31,22 @@
 #          using the y = mx + b format
 
 # write code that COMPUTES ANNUAL COMPOUND INTEREST
-#       0. takes in a initial amount of money, an interest rate (in between 0 and 1), and the number of years
+#       0. take in a initial amount of money, an interest rate (in between 0 and 1), and the number of years
 #       1. convert all values to floats using the float(...) function (its similar to how str(...) and int(...) are used)
 #       2. compute the resulting money after the provided number of years using the formula: A = P(1 + r)^n
 #               n = the number of years
 #               r = the interest rate in between 0 and 1
 #               A = the result after n years
-#       3. print: "After _ years, you will have $_" where the first _ is the number of years and the second _ is the resulting value
+#       3. print: "After _ years, you will have $_" where the first _ is the number of years and the second _ is the resulting value A
+
+# write code that FINDS THE AREA OF A PARALLELOGRAM
+#       0. take in 3 numbers through 3 separate calls to input(...) (the height of the parallelogram, the length of one side, the length of the other side)
+#       1. convert these to floats
+#       2. compute the area of the parallelogram formed by those lengths
+#       3. print the resulting area
+
+# write code that FINDS A POINT ON A QUADRATIC FUNCTION
+#       0. take in 3 numbers through 3 separate calls to input(...) (a, b, c) (where each are the respective constants in a quadratic function f(x) =a^2x + bx + c)
+#       1. convert these 3 numbers to floats
+#       2. take in 1 number through another call to input(...) which will be x and convert it to a float
+#       3. now compute f(x) and print the result
